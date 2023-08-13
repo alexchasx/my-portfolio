0b682ff6dc11103e125167afe1590befaa452d88
Alexander Chasovnikov <a.s.chasovnikov@yandex.ru>
2023-08-13 19:03:23 +0300

    configuring sass

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<script>
+  export default {}
+</script>
+
+<template>
+  <header class="header">
+    <div class="container header__container">
+
+      <div class="burger-wrap">
+        <button class="btn-reset burger" aria-label="Открыть меню">
+          <span class="burger__line"></span>
+          <span class="burger__line"></span>
+          <span class="burger__line"></span>
+        </button>
+      </div>
+
+      <nav class="nav header__nav" title="Главное меню">
+        <button class="btn-reset nav__close" aria-label="Закрыть меню"></button>
+
+        <ul class="list-reset nav__list">
+          <li class="nav__item">
+            <a href="#" class="nav__link">О&nbsp;студии</a>
+          </li>
+
+          <li class="nav__item">
+            <a href="#" class="nav__link">О&nbsp;студии2</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
