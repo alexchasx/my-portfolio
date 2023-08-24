d03d7b67ad0fd3cbbc4ec320b2a0e0471415adb1
Alexander Chasovnikov <a.s.chasovnikov@yandex.ru>
2023-08-24 14:35:12 +0300

    added custom cursor for header

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <script setup>
 import HeaderComponent from '@/components/HeaderComponent.vue';
 import ParallaxStars from '@/components/ParallaxStars.vue';
-// import { ref } from 'vue';
-// import IconSpaceShip from '@/components/icons/IconSpaceShip.vue';
+import { ref } from 'vue';
+import IconSpaceShip from '@/components/icons/IconSpaceShip.vue';
 
 // TODO: custom cursor?
-// const cursor = ref(null);
-// const moveCursor = (e) => {
-//   const mouseY = e.clientY;
-//   const mouseX = e.clientX;
-//   const cursorStyle = cursor.value.style;
+const cursor = ref(null);
+const moveCursor = (e) => {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+  const cursorStyle = cursor.value.style;
 
-//   if (mouseY > 76) {
-//     cursorStyle.display = 'none';
-//   } else {
-//     cursorStyle.display = 'block';
-//   }
-//   cursorStyle.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-// };
+  if (mouseY > 76) {
+    cursorStyle.display = 'none';
+  } else {
+    cursorStyle.display = 'block';
+  }
+  cursorStyle.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+};
 </script>
 
 <template>
   <div class="site-container">
     <parallax-stars />
 
-    <header-component />
-    <!-- <header-component @mousemove="moveCursor" /> -->
+    <!-- <header-component /> -->
+    <header-component @mousemove="moveCursor" />
 
     <main>
       <router-view />
@@ -33,7 +33,7 @@ import ParallaxStars from '@/components/ParallaxStars.vue';
   </div>
 
   <!-- TODO: custom cursor? -->
-  <!-- <div class="custom-cursor" ref="cursor">
+  <div class="custom-cursor" ref="cursor">
     <IconSpaceShip />
-  </div> -->
+  </div>
 </template>
